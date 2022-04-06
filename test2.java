b87a389612df46029e3d394d1f86a87c3e64c7bf
moulali <moulali123@gamil.com>
2022-04-06 13:46:10 +0530

    deleted data

--- test2.java
+++ test2.java
@@ -1,8 +1,7 @@
 public class test2 {
    static String a= "moulali";
    static String b="mahesh";
-    static String c="moosa";
     public static void main(String[] args){
-        System.out.println(a+b+c);
+        System.out.println(a+b);
     }
 }
