6860ea95472256b87e3dfa06432929658ad58783
moulali <moulali123@gamil.com>
2022-04-06 13:51:50 +0530

    ADDED DATS

--- test2.java
+++ test2.java
@@ -1,6 +1,7 @@
 public class test2 {
    static String a= "moulali";
    static String b="mahesh";
+   static String c ="sanker";
     public static void main(String[] args){
         System.out.println(a+b);
     }
