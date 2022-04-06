654316fe9a3eeb18324a0464c286938e0ca44778
moulali <moulali123@gamil.com>
2022-04-06 16:40:33 +0530

    a value is added here

--- multiply.java
+++ multiply.java
@@ -4,7 +4,7 @@ public class multiply {
     static int c;
     public static void mul(int a ,int b){
         c=a*b;
-        System.out.println("multiplication of two vlaues "+c);
+        System.out.println("multiplication of two vlaues "+c+a);
     }
     public static void main(String[] args){
         mul (10,5);
