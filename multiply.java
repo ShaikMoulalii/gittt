15bf439f98f9e4eb0de7261f331a2c01c7369a35
moulali <moulali123@gamil.com>
2022-04-06 16:31:52 +0530

    multiplication

---
+++ multiply.java
@@ -0,0 +1,12 @@
+public class multiply {
+    static int a ;
+    static int b;
+    static int c;
+    public static void mul(int a ,int b){
+        c=a*b;
+        System.out.println("multiplication of two vlaues "+c);
+    }
+    public static void main(String[] args){
+        mul (10,5);
+    }
+}
