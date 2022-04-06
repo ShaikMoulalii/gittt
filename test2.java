b9df6fb42f0e63dcbe85f7440405581693e346b7
moulali <moulali123@gamil.com>
2022-04-06 13:43:33 +0530

    my first commit

---
+++ test2.java
@@ -0,0 +1,8 @@
+public class test2 {
+   static String a= "moulali";
+   static String b="mahesh";
+    static String c="moosa";
+    public static void main(String[] args){
+        System.out.println(a+b+c);
+    }
+}
