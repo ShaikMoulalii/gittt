06b4f4cd4970f1bc38e78fe91100fd70882bb237
Palla Mahesh <pallam@maveric-systems.com>
2022-04-07 10:13:26 +0530

    rff

--- test2.java
+++ test2.java
@@ -1,8 +1,8 @@
 public class test2 {
    static String a= "moulali";
-   static String b="mahesh";
-   static String c ="sanker";
+   static String b="palla";
+   static String c =a+b;
     public static void main(String[] args){
-        System.out.println(a+b);
+        System.out.println(c);
     }
 }
